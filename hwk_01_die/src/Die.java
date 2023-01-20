92cb2a861d9c514d9ec93ea266b1fa77bcf67347
Thyago Mota <thyagomota@gmail.com>
2023-01-20 12:26:06 -0700

    updates

---
+++ hwk_01_die/src/Die.java
@@ -0,0 +1,36 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Student:
+ * Description: Homework 01 - Die
+ */
+
+import java.util.Random;
+
+public class Die {
+
+    private static final int MIN_SIDES     = 2;
+    private static final int DEFAULT_SIDES = 6;
+    private static final int INIT_VALUE    = 1;
+
+    public Die(int sides) {
+
+    }
+
+    public Die() {
+
+    }
+
+    public int getValue() {
+        return 0;
+    }
+
+    public void roll() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ hwk_01_die/src/DieDriver.java
@@ -0,0 +1,13 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Student:
+ * Description: Homework 01 - DieDriver
+ */
+
+public class DieDriver {
+
+    public static void main(String[] args) {
+
+    }
+}
