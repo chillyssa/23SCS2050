00e8320c12ad5a7f8c687858fc69c15bd6d28daa
Thyago Mota <thyagomota@gmail.com>
2023-01-26 15:01:15 -0700

    updates

---
+++ lab_01_bees/src/Bee.java
@@ -0,0 +1,15 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Students Names: 
+ * Description: Lab 01 - Bee
+ */
+
+public class Bee {
+
+    public static final int WORKER = 0;
+    public static final int DRONE  = 1;
+    public static final int QUEEN  = 2;
+
+
+}
---
+++ lab_01_bees/src/BeeDriver.java
@@ -0,0 +1,17 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Students Names: 
+ * Description: Lab 01 - BeeDriver
+ */
+
+import java.util.Random;
+
+public class BeeDriver {
+
+    public static void main(String[] args) {
+        final int TOTAL_BEES = 15;
+        
+
+    }
+}
