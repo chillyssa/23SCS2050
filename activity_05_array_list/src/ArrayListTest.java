5bade14512f5afeb2cbf2f3ca303660f48a68dd8
Thyago Mota <thyagomota@gmail.com>
2023-02-07 15:42:29 -0700

    updates

---
+++ activity_05_array_list/src/ArrayListTest.java
@@ -0,0 +1,17 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 05 - ArrayListTest
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+
+    // TODO: create a test case named testStringList to test all the methods in ArrayList using string as the data type
+    @Test
+    void testStringList() {
+
+    }
+}
