26e070161d5de479aa02345a6150cb797c2aa465
Thyago Mota <thyagomota@gmail.com>
2023-02-09 15:01:53 -0700

    updates

---
+++ activity_05_array_list/sol/ArrayListDriver.java
@@ -0,0 +1,29 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 05 - ArrayListDriver
+ */
+
+public class ArrayListDriver {
+
+    public static void main(String[] args) {
+
+        // TODOd: create an array list of strings and add "Joe", "Mary", and "Anna"; then print the list
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Joe");
+        names.add("Mary");
+        names.add("Anna");
+        names.add("bla");
+        names.add("bla");
+        names.add("bla");
+        names.add("bla");
+        names.add("bla");
+        names.add("bla");
+        names.add("bla");
+        names.add("ble");
+        System.out.println(names);
+
+        // TODO: create an array list of integers; then add 15 random elements; print the list
+
+    }
+}
