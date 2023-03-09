b058db3c51d6890799409dcd453fa5eaafe94f23
Thyago Mota <thyagomota@gmail.com>
2023-03-08 17:11:25 -0700

    updates

---
+++ activity_09_queues/src/Queue.java
@@ -0,0 +1,19 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 09 - Queue
+ */
+
+public class Queue<E> extends LinkedList<E> {
+
+    // TODO: implement method push for a queue
+    public void push(E value) {
+
+    }
+
+    // TODO: implement method pop for a queue
+    public E pop() throws Exception {
+        return null;
+    }
+
+}
