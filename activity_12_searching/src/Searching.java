72c5638d598264c365aa268ec729dec3ff1d94bf
Thyago Mota <thyagomota@gmail.com>
2023-03-10 14:22:24 -0700

    updates

---
+++ activity_12_searching/src/Searching.java
@@ -0,0 +1,30 @@
+/*
+ * CS 2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 12 - Searching Algorithms
+ */
+
+public class Searching {
+
+    // TODO #1: implement a linear search
+    static boolean lsearch(int data[], int el) {
+        return false;
+    }
+
+    // TODO #2: implement an optimized linear search (OK to assume that the array is sorted)
+    static boolean lsearch_optimized(int data[], int el) {
+        return false;
+    }
+
+    // TODO #3: implement a binary search (OK to assume that the array is sorted)
+    static boolean bsearch(int data[], int start, int end, int el) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        int data[] = { 3, 5, 1, 8, 12, 2, 7, 9 };
+        int sortedData[] = { 1, 2, 3, 5, 7, 8, 9, 12 };
+
+        // perform various search using the different search algorithms implemented above
+    }
+}
