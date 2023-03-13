a882eb8201134b1d576594388c2e4fe639d44872
Thyago Mota <thyagomota@gmail.com>
2023-03-13 17:03:46 -0600

    updates

---
+++ activity_13_sorting/src/Sorting.java
@@ -0,0 +1,52 @@
+/*
+ * CS 2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 13 - Sorting Algorithms
+ */
+
+import java.util.Arrays;
+
+public class Sorting {
+
+    // TODO #1: implement the bubble sort algorithm
+    public static void bubbleSort(int data[]) {
+        
+    }
+
+    // TODO #2: implement the selection sort algorithm
+    public static void selectionSort(int data[]) {
+        
+    }
+
+    // TODO #3: implement the insertion sort algorithm
+    public static void insertionSort(int data[]) {
+        
+    }
+
+
+    // TODO #4: implement the partition part of the quick sort algorithm
+    public static int partition(int data[], int start, int end) {
+        return 0;
+    }
+
+    // TODO #5: implement the quick sort algorithm
+    public static void quickSort(int data[], int start, int end) {
+        
+    }
+
+    // TODO #6: implement the merge part of the merge sort algorithm
+    public static void merge(int data[], int begin, int middle, int end) {
+        
+    }
+
+    // TODO #7: implement the merge sort algorithm
+    public static void mergeSort(int data[], int begin, int end) {
+        
+    }
+
+    public static void main(String[] args) {
+         int data[] = {13, 12, 84, 79, 10, 77, 56, 1, 34, 27, 3};
+        // perform various sorts using the different sorting algorithms implemented above
+    }
+    
+}
