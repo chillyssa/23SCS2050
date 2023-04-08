e91e57e81ab53f2807b794834db21576c13db771
Thyago Mota <thyagomota@gmail.com>
2023-04-08 16:56:34 -0600

    updates

---
+++ activity_14_bst/src/BST.java
@@ -0,0 +1,68 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 14 - BST class
+ */
+
+import java.util.Iterator;
+
+public class BST<E> {
+
+    private BSTNode<E> root;
+
+    public BST() {
+        root = null;
+    }
+
+    // TODO: implement the isEmpty method
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // TODO: implement the addRecursively private method
+    private void addRecursively(BSTNode<E> current, final E value) {
+
+
+    }
+
+    // TODO: implement the add method
+    public void add(final E value) {
+
+    }
+
+    // TODO: override the toString method
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    // TODO: implement searchRecursively
+    private boolean searchRecursively(final BSTNode<E> current, final E value) {
+        return false;
+    }
+
+    // TODO: implement search
+    public boolean search(final E value) {
+        return false;
+    }
+
+    // TODO: implement numberChildren
+    private int numberChildren(final BSTNode<E> current) {
+        return 0;
+    }
+
+    // TODO: implement getLeftMost
+    private BSTNode<E> getLeftMost(final BSTNode<E> current) {
+        return null;
+    }
+
+    // TODO: implement removeRecursively
+    private BSTNode<E> removeRecursively(final BSTNode<E> current, final E value) {
+        return null;
+    }
+
+    // TODO: implement remove
+    public void remove(final E value) {
+
+    }
+}
---
+++ activity_14_bst/src/BSTDriver.java
@@ -0,0 +1,15 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 14 - BSTDriver class
+ */
+
+import java.util.Iterator;
+
+public class BSTDriver {
+
+    // TODO: create a binary tree and add the data elements to reproduce the tree described in the lecture about trees; don't forget to print your tree at the end
+    public static void main(String[] args) {
+
+    }
+}
